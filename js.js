ecae511bbb6105e73cc5097dd2678d8223eb146d
Luann Rodrigo de Souza Santos <94115637+luannsantos321@users.noreply.github.com>
2022-12-21 09:22:29 +0000

    alterando animação lado esquerdo

--- js.js
+++ js.js
@@ -1,22 +1,16 @@
+document.getElementById("resultado").innerHTML = None
+ function validNumber(){
+ recebe = document.getElementById("senha");
+ let resultNumber  = recebe.value.match(/[0-9]/g);
+console.log(resultNumber);
 
 
-function password(){
 
-    const numeros = ["1","2","3","4","5","6","7","8","9", "0"];
-    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
-    "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    if (null){
+if (!resultNumber){
+   document.getElementById("resultado").innerHTML = "Deve conter numeros"
+}else{
+    document.getElementById("resultado").innerHTML = " "
+}
 
-    } ;
-    if (alfabeto.includes("senha")){
-        alert.log("ok");
-    }else{
-        document.getElementById("resultado").innerHTML = "Deve conter letra";
-    };
-    if (numeros.includes("senha")){
-       alert("ok");
-    }else{
-        document.getElementById("resultado").innerHTML = "deve conter um numero";
-    };
-};
-password();
+ }
+ validNumber()
